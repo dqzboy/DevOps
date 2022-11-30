785e2d524eaabc58eeb858c96c4947db47a28ca5
dqzboy <42825450+dqzboy@users.noreply.github.com>
2022-11-30 13:38:07 +0800

    Update library.groovy

--- library/library.groovy
+++ library/library.groovy
@@ -1,18 +1,17 @@
 package org.devops
-def Ding() {
+def Ding() { 
 	sh """
-		curl --location --request POST 'hppts://oapi.dingtalk.com/robot/send?access_token=<webhook>' \
-		--header 'Content-Type: application/json' \
-		--data-raw '{
+	    curl --location --request POST 'https://oapi.dingtalk.com/robot/send?access_token=<you token>' \
+	    --header 'Content-Type: application/json' \
+	    --data-raw '{
             "msgtype": "markdown",
             "markdown": {
                 "title": "项目构建信息",
-                "text": "## 消息1 \n- 消息2 \n- 消息3 \n- 消息4"
+                "text": "### rom-test 生产发布申请,请【${adminUser}】审批@${approvalDD}\n- 应用名称: ${env.JOB_NAME}\n- 随机验证码: ${randomToken}"
             },
             "at": {
                 "isAtAll": false
             }
         }'
-
 	"""
 }
